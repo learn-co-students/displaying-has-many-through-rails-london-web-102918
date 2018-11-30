db16e7b49e95f2cc20bd2b960400a9ef7216cf60
rosen777 <rosen.toshev@flatironschool.com>
2018-11-30 15:31:13 +0000

    Done.

--- db/migrate/20160114212908_create_users.rb
+++ db/migrate/20160114212908_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :username
-      t.string :email
+    create_table :comments do |t|
+      t.string :content
+      t.belongs_to :user
+      t.belongs_to :post
       t.timestamps null: false
     end
   end
